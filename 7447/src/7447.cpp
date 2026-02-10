cd34a63fca4a7e1eeecd0038301dadcc4ab510b8
pranjalroy19 <pranjalroy111sp@gmail.com>
2026-02-10 10:15:15 +0000

    fsm

---
+++ 7447/src/7447.cpp
@@ -0,0 +1,25 @@
+#include<Arduino.h>
+
+void disp_7447(int D, int C, int B, int A)
+{
+  digitalWrite(2, A); //LSB
+  digitalWrite(3, B); 
+  digitalWrite(4, C); 
+  digitalWrite(5, D); //MSB
+
+}
+// the setup function runs once when you press reset or power the board
+void setup() {
+    pinMode(2, OUTPUT);  
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+}
+
+void loop() {
+
+  disp_7447(0,1,1,1); delay(1000);
+  disp_7447(0,1,0,0); delay(1000);
+  disp_7447(0,1,0,0); delay(1000);
+  disp_7447(0,1,1,1); delay(1000);
+}
